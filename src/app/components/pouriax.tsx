958cde977e4cf7a6652cba1093fc7f4903e133a6
Pouria <pooriaramezani1382@gmail.com>
2025-07-15 13:34:09 +0330

    upload first version

---
+++ src/app/components/pouriax.tsx
@@ -0,0 +1,11 @@
+export default function Pouriax(){
+    return(
+        <div className="min-h-screen flex justify-center items-center">
+            
+            <h1 className="text-8xl font-black mb-30 hover:cursor-default">Pouria</h1>
+            <h3 className="text-4xl font-black hover:cursor-default mb-18 p-1 bg-gradient-to-r animated-gradient-text">X</h3>
+            
+            
+        </div>
+    )
+}
