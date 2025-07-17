9fc92d89e2f3111b1fd91ae1b86eef4fc5b5241a
Pouria <pooriaramezani1382@gmail.com>
2025-07-17 22:47:21 +0330

    add second page

--- src/app/components/pouriax.tsx
+++ src/app/components/pouriax.tsx
@@ -11,11 +11,13 @@ gsap.registerPlugin(ScrollTrigger);
 export default function Pouriax(){
 
     const menu = ['Home','About','Skills','Projects','Contact']
-    const scrollRef = useRef(null);
+
+    const scrollRef1 = useRef(null);
+    const scrollRef2 = useRef(null);
 
 
     useGSAP((()=>{
-        const Name = scrollRef.current;
+        const Name = scrollRef1.current;
         gsap.to(Name,{
             x:-800,
             opacity:0,
@@ -26,20 +28,42 @@ export default function Pouriax(){
                 end:"bottom top"
             },
             ease:"sine.inOut"
-        })
+        });
+        const SecondPart = scrollRef2.current;
+        gsap.fromTo(SecondPart,
+            { x: '45%', opacity: 0 },
+            {
+              x: '0%',
+              opacity: 1,
+              scrollTrigger: {
+            trigger: SecondPart,
+            start: 'top bottom%',
+            end: 'bottom -20%',
+            scrub: true,
+              },
+              ease:"sine.inOut"
+            }
+            
+          );
         
     }),[]);
 
 
     return(
-        <div className="pagesize flex justify-center items-center">
+        <div className="min-h-[500vh] flex justify-center items-center">
             
-            <div ref={scrollRef} className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-full px-4">
+            <div ref={scrollRef1} 
+                className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-full px-4">
                 <div className="flex items-center justify-center">
                     <h1 className="text-6xl sm:text-7xl md:text-8xl font-black hover:cursor-default mb-6 sm:mb-8 md:mb-10">Pouria</h1>
                     <h3 className="text-3xl sm:text-4xl md:text-5xl mb-1 font-black hover:cursor-default p-1 bg-gradient-to-r animated-gradient-text ">X</h3>
                 </div>
             </div>
+
+            <div ref={scrollRef2} className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-full px-4 opacity-0">
+                <h1 className='text-4xl'> SecondPart</h1>
+            </div>
+
             <div className="fixed bottom-0 mb-6 sm:mb-8 md:mb-12 w-full text-center px-4">
                 <div className="flex flex-wrap justify-center gap-2 sm:gap-4">
                     <a className="text-sm mb-10 sm:text-lg md:text-xl opacity-60 hover:opacity-100 transition-all hover:scale-110 hover:cursor-pointer">{menu.at(0)}</a>
